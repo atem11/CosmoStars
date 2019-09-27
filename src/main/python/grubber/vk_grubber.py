ebfd6cce9d9cb10a2818a85621b9e36d13e7f5be
atem11 <atem11@yandex-team.ru>
2019-09-27 21:37:18 +0300

    python

--- src/main/python/grubber/vk_grubber.py
+++ src/main/python/grubber/vk_grubber.py
@@ -1,14 +1,14 @@
-import vk
+import vk_api as vk
 
 
 class Grubber:
 
     def __init__(self, vk_token: str):
         self.token = vk_token
-        self.session = vk.Session(access_token=self.token)
-        self.api = vk.API(self.session)
-        pass
+        self.session = vk.VkApi(token=self.token)
+        self.session.auth(token_only="true")
+        self.api = self.session.get_api()
 
-    def print_info(self, addr: str):
-        print(self.api.users.get(user_ids=1))
+    def print_info(self, vk_id: str):
+        print(self.api.wall.get(domain=vk_id, cout=5))
         return
