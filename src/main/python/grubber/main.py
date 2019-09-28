d322aa86ea000c5601e9776c391d5f6de9f43738
atem11 <atem11@yandex-team.ru>
2019-09-28 11:09:06 +0300

    grub data

--- src/main/python/grubber/main.py
+++ src/main/python/grubber/main.py
@@ -1,4 +1,5 @@
 import configparser
+import json
 
 from grubber.vk_grubber import Grubber
 
@@ -9,7 +10,18 @@ def main():
     vk_token = config['DEFAULT']['vk_token']
 
     test = Grubber(vk_token)
-    test.print_info("olgabuzova")
+    ids = open("../../resources/celebrities_id.txt", "r")
+    result = []
+    cnt = 0
+    for celeb_id in ids:
+        cnt += 1
+        print("grub celeb num=" + str(cnt) + " id=" + celeb_id)
+        res = test.posts(int(celeb_id), 1564617600)  # 2019/august/01 00:00:00
+        result.extend(res)
+
+    result = sorted(result, key=lambda post: post['date'], reverse=True)
+    out = open("../../resources/celeb_posts.json", "w")
+    out.write(json.dumps(result, indent=2, ensure_ascii=False))
 
 
 if __name__ == "__main__":
--- src/main/python/grubber/vk_grubber.py
+++ src/main/python/grubber/vk_grubber.py
@@ -1,5 +1,5 @@
 import vk_api as vk
-import json
+
 
 class Grubber:
 
@@ -8,7 +8,31 @@ class Grubber:
         self.session = vk.VkApi(token=self.token)
         self.api = self.session.get_api()
 
-    def print_info(self, vk_id: str):
+    def posts(self, vk_id: int, timestamp: int):
         user_id = self.api.users.get(user_ids=[vk_id])[0]['id']
-        print(json.dumps(self.api.wall.get(owner_id=-user_id, offset=148273753, extended=1), indent=2))
-        return
+        try:
+            post_list = self.api.wall.get(owner_id=user_id, count=100, filter="owner")
+            ans_list = []
+            for post in post_list['items']:
+                if int(post['date']) > timestamp:
+                    item = {'id': post['id'],
+                            'owner_id': post['owner_id'],
+                            'date': post['date'],
+                            'text': post['text'],
+                            'attachments': []
+                            }
+                    if 'attachments' in post:
+                        item['attachments'] = post['attachments']
+                    elif 'copy_history' in post:
+                        item['attachments'].append({'type': 'repost',
+                                                    'repost': {
+                                                        'id': post['copy_history'][0]['id'],
+                                                        'owner_id': post['copy_history'][0]['owner_id']
+                                                        }
+                                                    })
+                    ans_list.append(item)
+
+            return ans_list
+        except vk.exceptions.ApiError:
+            print("wall closed " + str(vk_id))
+            return []
